65ee73a62c519e67b7b3f704cb4415a07be4e5e1
Esraa <jbesraa@gmail.com>
2016-10-26 12:38:21 +0100

    add 2nd test

--- spec/tests.js
+++ spec/tests.js
@@ -1,3 +1,7 @@
 QUnit.test( "Testing time before stopwatch starts", function( assert ) {
   assert.strictEqual(stopWatch.getTime(), '00:00:00', "Passed!" );
 });
+
+QUnit.test( "Testing time before stopwatch starts", function( assert ) {
+  assert.strictEqual(stopWatch.isActive(), false, "Passed!" );
+});
