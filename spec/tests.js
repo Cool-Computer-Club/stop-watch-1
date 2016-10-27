5b705be067953ebd2655df8abdd7b17e00ece98d
John Whiles <johnwhiles@outlook.com>
2016-10-27 15:41:23 +0100

    adds a lot of tests for DOm manipulation

--- spec/tests.js
+++ spec/tests.js
@@ -26,6 +26,43 @@ QUnit.test( "testing if we can reset the time", function( assert ) {
   assert.strictEqual(stopWatch.getTime(), '00:00:00:000', "Passed!" );
 });
 QUnit.test("test resetTime on the DOM", function(assert){
+  document.getElementsByClassName('display')[0].innerText = 'whatever';
   stopWatch.resetTime();
   assert.strictEqual(document.getElementsByClassName('display')[0].innerText, '00:00:00:000', "Passed!");
+});
+
+QUnit.test('tests if we can click on start and have it start', function (assert) {
+  stopWatch.stopTime();
+  console.log('called stop ' + active);
+  document.getElementsByClassName('start_btn')[0].click();
+  console.log('clicked start ' + active);
+  assert.strictEqual(active, true, "passed!" );
+});
+
+QUnit.test('tests if we can click on stop and have it stop', function (assert) {
+  document.getElementsByClassName('stop_btn')[0].click();
+  assert.strictEqual(active, false, "passed!" );
+});
+
+QUnit.test('tests if we can click on reset and have it update the DOM', function (assert) {
+  document.getElementsByClassName('display')[0].innerText = 'whatever';
+  document.getElementsByClassName('reset_btn')[0].click();
+  assert.strictEqual(document.getElementsByClassName('display')[0].innerText, '00:00:00:000', "Passed!");
+});
+
+QUnit.test('tests if we can click on reset and have it update stop', function (assert) {
+  stopWatch.startTime();
+  document.getElementsByClassName('reset_btn')[0].click();
+  assert.strictEqual(active, false, "passed!");
+});
+
+
+QUnit.test("Asynch Test", function( assert ) {
+  var done = assert.async();
+  stopWatch.resetTime();
+  stopWatch.startTime();
+  setTimeout(function() {
+    assert.strictEqual(stopWatch.seconds, '03', "got the right time!");
+    done;
+  }, 3100);
 });
